362c97fe0e61ea76bc6b05907ac4264db7142d85
Guillem Mateos <bbguillem@gmail.com>
2019-02-05 18:46:42 +0100

    Add error message for stack status

--- commands/status.js
+++ commands/status.js
@@ -2,7 +2,12 @@ status_actions = require("../actions/status")
 
 exports.command = 'status'
 exports.desc = 'Show stack status'
-exports.builder = {}
+exports.builder = (yargs) => {
+  return yargs.fail(function(msg,err) {
+    if( err )
+      console.error('Please make sure to run the stack command inside a stack folder')
+  });
+}
 exports.handler = async (argv) => {
   await status_actions();
 }
