02b34e54cc781833e018184d9367b0307811a7ee
Guillem Mateos <bbguillem@gmail.com>
2019-02-12 19:02:18 +0100

    Improve reporting on failed furnace ignite

--- commands/ignite.js
+++ commands/ignite.js
@@ -2,7 +2,11 @@ ignite_actions = require("../actions/ignite")
 
 exports.command = 'ignite'
 exports.desc = 'Initialize a new Furnace instance'
-exports.builder = {}
+exports.builder = (yargs) => {
+  return yargs.fail(function(msg,err) {
+    console.error('furnace ignite failed:', err);
+  });
+}
 exports.handler = async (argv) => {
   await ignite_actions();
 }
