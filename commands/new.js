be0cc6c569aabbbbbf6a8b7b85bf9c605fc7f12e
Guillem Mateos <bbguillem@gmail.com>
2019-02-13 12:59:10 +0100

    Avoid swallowing error message on new

--- commands/new.js
+++ commands/new.js
@@ -6,6 +6,8 @@ exports.builder = (yargs) => {
   return yargs.fail(function(msg,err) {
     if( err && err.name === 'HttpError' && err.status === 401 )
       console.error('GitHub authentication failed. You may want to check your GitHub token.')
+    else
+      console.error(err);
   });
 }
 exports.handler = async (argv) => {
