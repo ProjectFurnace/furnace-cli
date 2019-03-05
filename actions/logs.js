afee3988bf81568d57566cdc828043b1907c2e98
Danny Waite <danny@dannys-mbp-2.lan>
2019-03-05 13:41:21 +0000

    furnace logs command for tailing cloudwatch logs

---
+++ actions/logs.js
@@ -0,0 +1,40 @@
+const workspace = require("../utils/workspace")
+    , context = workspace.getCurrentContext()
+    , stackUtils = require("../utils/stack")
+    , fsUtils = require("@project-furnace/fsutils")
+    , path = require("path")
+    , chalk = require("chalk")
+    , CWLogFilterEventStream = require("smoketail").CWLogFilterEventStream
+    ;
+
+module.exports = async (env, name) => {
+
+  const stackFile = path.join(process.cwd(), "stack.yaml");
+  if (!fsUtils.exists(stackFile)) {
+    console.error("you must be inside a furance stack directory");
+    return;
+  }
+  const stack = stackUtils.getConfig("stack")
+      , logGroupName = `/aws/lambda/${stack.name}-${name}-${env}`
+      , filterOpts = {
+          logGroupName,
+          follow: true
+        }
+      , awsOpts = { region : context.region  }
+      ;
+      console.log(filterOpts, awsOpts)
+
+  const eventStream = new CWLogFilterEventStream(filterOpts,awsOpts);
+  eventStream.on('error', function(err){
+      console.log(chalk.red(err));
+      eventStream.close();
+  });
+
+  eventStream.on('data',function(eventObject){
+      console.log(
+        chalk.cyan(new Date(eventObject.timestamp)),
+        chalk.white(eventObject.message)
+      );
+  });
+
+}
