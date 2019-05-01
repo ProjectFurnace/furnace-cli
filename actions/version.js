ee8f19ef164dc8328bca719eeebf3b3517fa49ec
Danny Waite <danny@Dannys-MacBook-Pro-2.local>
2019-05-01 20:24:57 +0100

    add version command

---
+++ actions/version.js
@@ -0,0 +1,28 @@
+const getPackageVersion = require("get-pkg-version")
+    , { readlinkSync } = require("fs")
+    , which = require("which")
+    , path = require("path")
+    , fsutils = require("@project-furnace/fsutils")
+    ;
+
+module.exports = async () => {
+
+
+  try {
+    const version = await getPackageVersion("@project-furnace/furnace-cli");
+    console.log(version);
+  } catch (err) {
+    const binPath = which.sync("furnace");
+    const linkPath = readlinkSync(binPath);
+    const resolvedPath = path.resolve(binPath, "..", linkPath)
+    const baseDir = path.dirname(resolvedPath);
+    const packageJson = path.join(baseDir, "package.json");
+
+    if (fsutils.exists(packageJson)) {
+      const package = JSON.parse(fsutils.readFile(packageJson));
+      console.log(package.version);
+    } else {
+      console.log(`unable to find package.json at ${packageJson}`);
+    }
+  }
+}
---
+++ commands/version.js
@@ -0,0 +1,7 @@
+status_actions = require("../actions/version")
+
+exports.command = 'version'
+exports.desc = 'Show current Furnace CLI Version'
+exports.handler = async (argv) => {
+  status_actions();
+}
