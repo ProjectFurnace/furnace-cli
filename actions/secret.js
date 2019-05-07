dd5d74421daed85882b6b00edb4e139a475d4304
Danny Waite <dannywaite7@gmail.com>
2019-05-07 20:39:18 +0100

    secrets now use SSM deploy local now gets instance name

--- actions/secret.js
+++ actions/secret.js
@@ -1,75 +1,92 @@
-const AWS = require("../utils/aws").getInstance
-    , stack = require("../utils/stack")
-    ;
+const stack = require("../utils/stack");
+
+module.exports.get = async (environment, name) => {
+  const ssm = getSSM()
+      , secretName = getSecretFullName(environment, name)
+      , secret = await getSecret(ssm, secretName)
+      ;
+
+  if (secret) {
+    console.log(secret);
+  } else {
+    console.error(`unable to retrieve secret ${name}`);
+  }
+}
 
 module.exports.add = async (environment, name, secret) => {
-    const aws = AWS();
 
-    if( !environment || !name || !secret ) {
-        console.error(`Parameters missing. Please use 'furnace secret add [env] [name] [secret]`);
-        return;
-    }
-    const stackConfig = stack.getConfig("stack")
-        , workspace = require("../utils/workspace")
-        , stackName = stackConfig.name
-        , context = workspace.getCurrentContext()
-        ;
+  const ssm = getSSM()
+      , secretName = getSecretFullName(environment, name)
+      ;
 
-    const sm = new aws.SecretsManager();
+  const params = {
+    Name: secretName,
+    Type: "SecureString",
+    Value: secret,
+    Overwrite: true
+  };
 
-    const secretFullName = `${context.name}/${stackName}-${name}-${environment}`;
-
-    const existingSecrets = sm.listSecrets({}).promise();
-
-    if (existingSecrets.SecretList ) {
-        for (let secret of existingSecrets.SecretList) {
-            if (secret.Name == secretFullName) {
-                console.log(`Secret ${name} already exists. If you want to recreate it, delete it first`);
-                return;
-            }
-        }
-    }
-
-    const params = {
-        Name: secretFullName,
-        SecretString: secret
-    };
-    
-    try {
-        await sm.createSecret(params).promise();
-        console.log(`Secret ${name} for environment ${environment} created successfully`);
-    } catch(e) {
-        console.log(`Secret ${name} for environment ${environment} already exists`);
-    }
+  try {
+    await ssm.putParameter(params).promise();
+    console.log(`Secret ${name} for environment ${environment} created successfully`);
+  } catch (error) {
+    console.log(`error adding secret ${secretName}`, error);
+  }
 }
 
 module.exports.del = async (environment, name) => {
-    const aws = AWS();
 
-    if( !environment || !name ) {
-        console.error(`Parameters missing. Please use 'furnace secret del [env] [name]`);
-        return;
-    }
+  const ssm = getSSM()
+      , secretName = getSecretFullName(environment, name)
+      , secret = await getSecret(ssm, secretName)
+      ;
 
-    const stackConfig = stack.getConfig("stack")
-        , workspace = require("../utils/workspace")
-        , stackName = stackConfig.name
-        , context = workspace.getCurrentContext()
-        ;
+  if (!secret) {
+    console.log(`secret does not currently exist`);
+    process.exit(2);
+  }
 
-    const sm = new aws.SecretsManager();
+  const params = {
+    Name: secretName
+  };
 
-    const secretFullName = `${context.name}/${stackName}-${name}-${environment}`;
+  try {
+    await ssm.deleteParameter(params).promise();
+    console.log(`Secret ${name} for environment ${environment} deleted successfully`);
+  } catch (error) {
+    console.log(`error deleting secret ${secretName}`, error);
+  }
+}
 
+async function getSecret(ssm, secretName) {
+  try {
     const params = {
-        SecretId: secretFullName,
-        ForceDeleteWithoutRecovery: true
+      Name: secretName,
+      WithDecryption: true
     };
-    
-    try {
-        await sm.deleteSecret(params).promise();
-        console.log(`Secret ${name} for environment ${environment} deleted successfully`);
-    } catch( e ) {
-        console.error(`Secret ${name} not found in environment ${environment}`);
-    }
+
+    const result = await ssm.getParameter(params).promise();
+    return result.Parameter.Value;
+  } catch (error) {
+    return null;
+  }
+}
+
+function getSecretFullName(environment, name) {
+  const stackConfig = stack.getConfig("stack")
+      , workspace = require("../utils/workspace")
+      , stackName = stackConfig.name
+      , context = workspace.getCurrentContext()
+      ;
+
+  return `/${context.name}/${stackName}/${name}/${environment}`;
+}
+
+function getSSM() {
+  const AWS = require("../utils/aws").getInstance
+      , aws = AWS()
+      , ssm = new aws.SSM()
+      ;
+
+  return ssm;
 }
