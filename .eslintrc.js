b902a3c1e2e085cf15ce51d564dfce6181a7965c
Chris Pynegar <chrispynegar2157@gmail.com>
2019-05-08 22:25:35 +0100

    Tidy up eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  extends:  [
-    'airbnb-typescript/base',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-typescript/base',
   ],
- parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
+ parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  rules:  {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  rules: {
     'no-console': 'off'
   },
 };
