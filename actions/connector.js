76f862f273238b1a22074e19df6b935830f9ab8c
DeathsPirate <davemound@gmail.com>
2019-09-25 05:23:44 -0700

    Added functionality for new config format

--- actions/connector.js
+++ actions/connector.js
@@ -7,33 +7,107 @@ const workspace = require("../utils/workspace")
     , path = require("path")
     , util = require("util")
     , fluentd = require("../utils/connectors/fluentd/fluentd")
-    ;
+    , AWS = require("aws-sdk");
+;
 
 module.exports.add = async (name) => {
 
-   
-
+    const context = workspace.getCurrentContext();
     const connectors = stack.getConfig("connectors");
     const connector = connectors.find(c => c.name === name);
-    if(connector.type == "FluentConnector"){    
-        try {
-            fluentd.get_template(connector)
-        } catch (error) {
-            console.log(error);
+    if (connector.type == "FluentConnector") {
+        if (context.platform == "aws") {
+            try {
+                await buildAWSOutputs(connector, context);
+            } catch (error) {
+                console.log(chalk.red(error));
+            }
+        } else {
+            console.log(chalk.red("No definition template for " + context.platform))
         }
     }
 
 }
 
-function stateToColour(state) {
-    switch (state) {
-        case "success":
-        return chalk.green(state);
-        case "in_progress":
-            return chalk.yellow(state);
-        case "failed":
-            return chalk.red(state);
-        default:
-            return chalk.red(state);
-    }
+async function buildAWSOutputs(connector, context) {
+    AWS.config.update({ region: context.region })
+    const stack_info = stack.getConfig("stack");
+    const sources = stack.getConfig("sources");
+    const envs = stack_info.environments;
+    const sts = new AWS.STS();
+    let account_id = "";
+    let account_promise = sts.getCallerIdentity({}).promise();
+    await account_promise.then(
+        function (data) {
+            account_id = data.Account;
+        },
+        function (error) {
+            console.log(chalk.red(error));
+        }
+
+    )
+    const url_base = "https://sqs." + context.region + ".amazonaws.com/" + account_id + "/"
+    envs.forEach(env => {
+        connector.config.outputs.forEach(output => {
+            const source = sources.find(s => s.name === output.source);
+            const queuename = stack_info.name + '-' + output.source + '-' + env;
+            if (!source) {
+                console.log(chalk.red("No source found for source name: " + output.source));
+            }
+            else {
+                if (!("options" in output)) {
+                    output.options = {}
+                }
+
+                if (!("match" in output.options)) {
+                    output.options.match = []
+                }
+
+                switch (source.type) {
+                    case "aws.sqs.Queue":
+
+                        const queueUrl = url_base + queuename
+
+                        const sqs_match = {
+                            'match_pattern': output.name,
+                            '@type': "sqs",
+                            'sqs_url': queueUrl,
+                            'region': context.region,
+                            'aws_key_id': '[REPLACE WITH AWS KEY]',
+                            'aws_sec_key': '[REPLACE WITH AWS SECRET]',
+                            'include_tag': true,
+                            'tag_property_name': 'tag',
+                        }
+                        if (output.options.match[0]) {
+                            Object.assign(output.options.match[0], sqs_match);
+                        } else {
+                            output.options.match.push(sqs_match);
+                        }
+                        break;
+
+                    default:
+                        const kinesis_match = {
+                            'match_pattern': output.name,
+                            '@type': 'kinesis_streams',
+                            'stream_name': queuename,
+                            'region': context.region,
+                            'aws_key_id': '[REPLACE WITH AWS KEY]',
+                            'aws_sec_key': '[REPLACE WITH AWS SECRET]'
+                        }
+                        if (output.options.match[0]) {
+                            Object.assign(output.options.match[0], kinesis_match);
+                        } else {
+                            output.options.match.push(kinesis_match);
+                        }
+                        break;
+                }
+
+            }
+
+        })
+        fluentd.get_template(connector, connector.name, env);
+    })
+
 }
+
+
--- utils/connectors/fluentd/fluentd.js
+++ utils/connectors/fluentd/fluentd.js
@@ -1,40 +1,49 @@
 const nc = require('nunjucks');
 const path = require('path');
 const fs = require('fs');
-
-function getTemplate(type){
-    const parts = type.split('.');
-    return path.join('.', parts[0], parts[1] + '.njk');
+const chalk = require('chalk');
+function getTemplate(type) {
+    return './aws/default.njk';
 }
 
-function renderTemplate(type, data)
-{
+function renderTemplate(type, data) {
     return nc.render(type, data);
 }
 
-function checkDir(){
+function checkDir() {
     const dir = './config_files';
-    if(!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
 }
 
-function saveFile(filename, contents){
-    fs.writeFile("./config_files/" + filename + ".conf", contents, function(err) {
-        if(err) {
-            return console.log(err);
+function saveFile(filename, contents) {
+    fs.writeFile("./config_files/" + filename + ".conf", contents, function (err) {
+        if (err) {
+            return console.log(chalk.red(err));
         }
-    
-        console.log('Saved ' + filename + ' configuration successfully.');
+
+        console.log(chalk.green('Saved ') + chalk.blue(filename) + chalk.green(' configuration to the /config_files directory.'));
     });
 }
 
-module.exports.get_template = (connector) => {
-    checkDir(); 
-    connector.config.inputs.forEach(input => {
-             nc.configure(__dirname + '/templates', {trimBlocks: true});
-             const completed_template = renderTemplate(getTemplate(input.type), input.options);
-             saveFile(input.name, completed_template.replace(/&quot;/g, "\""))
+module.exports.get_template = (connector, name, environment) => {
+    checkDir();
+    let completed_template = "";
+    let filename = name + '_' + environment;
 
-             })
+    connector.config.inputs.forEach(input => {
+        nc.configure(__dirname + '/templates', { trimBlocks: true });
+        out = renderTemplate(getTemplate(input.type), input.options);
+        completed_template += out.replace(/&quot;/g, "\"").replace(/&#39;/g, "\'")
+    })
+
+    connector.config.outputs.forEach(output => {
+        nc.configure(__dirname + '/templates', { trimBlocks: true });
+        out = renderTemplate(getTemplate(output.type), output.options);
+        completed_template += out.replace(/&quot;/g, "\"").replace(/&#39;/g, "\'")
+    })
+
+    saveFile(filename, completed_template)
+    //console.log(completed_template);
 };
